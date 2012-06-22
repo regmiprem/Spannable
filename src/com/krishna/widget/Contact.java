398c06f08d4908252b5d6e5173c23d96a56aefe8
regmiprem <regmiprem101@gmail.com>
2012-06-22 10:25:39 +0545

    Update master

--- src/com/krishna/widget/Contact.java
+++ src/com/krishna/widget/Contact.java
@@ -5,7 +5,7 @@ public class Contact {
 	
 	public String contactName;
 	public String num;
-	public Boolean isContactChecked = false,isHeader=false;
+	public Boolean isContactChecked = false
 	
 	
 
